1e88297c85cc16082596aa8cb24d95c478728021
simonetravaglini1 <simonetravaglini@studenti.unimi.it>
2022-12-13 11:42:41 +0100

    tryng to import data from my twitter list

---
+++ script/script2_importdatafromtwitter.R
@@ -0,0 +1,35 @@
+# i create 4 list on my twitter account. one for cdx coalition,
+# csx coalition, m5s and terzo polo coalition. all the lists contain politicians twitter account 
+# the next task will be the parse of tweet during the election campaign.
+#for each coalition, which were the main topic or argoument that account speach about?
+
+#first step: import data from list.
+auth_as('simonetrvgln')
+
+library(rtweet)
+library(tidyverse)
+library(dplyr)
+
+
+auth_setup_default()
+
+italian_cdx = lists_members(
+  list_id = "1602429027561750528",
+  owner_user = NULL,
+  slug = NULL,
+  n = 17,
+  cursor = "-1",
+  token = NULL,
+  retryonratelimit = TRUE,
+  parse = TRUE,
+  verbose = TRUE,
+  
+)
+
+
+
+
+
+
+
+
