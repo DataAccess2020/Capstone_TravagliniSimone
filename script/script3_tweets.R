3cb76a01bbd7f6df10a3a7ccfa0300bafa9ade57
simonetravaglini1 <simonetravaglini@studenti.unimi.it>
2022-12-14 17:49:19 +0100

    tried to extract all the tweets

--- script/script3_tweets.R
+++ script/script3_tweets.R
@@ -1,7 +1,8 @@
 ## next step, trying to download all the tweet from our lists.
 library(dplyr)
+
 ##prova 1: with tweets from 25th of july to 24th of september for dx coalition
-dxcoalitions_tweet <- rtweet::get_timeline(retryonratelimit = T,
+dxcoalition_tweet <- rtweet::get_timeline(retryonratelimit = T,
   user = list_italian_cdx, 
   since_id = NULL,
   max_id = NULL,
@@ -10,4 +11,20 @@ dxcoalitions_tweet <- rtweet::get_timeline(retryonratelimit = T,
   ) %>% 
   dplyr::filter(created_at > "2022-07-25" & created_at <="2022-09-24")
 
-## it retourns me a list with only 463 tweets. probably there are some issues
+## it retorns me a list with only 463 tweets. probably there are some issues
+## now i try with m56
+list_italian_m5s <- c(italian_m5s$screen_name)
+list_italian_m5s
+
+m5s_tweet <- rtweet::get_timeline(retryonratelimit = T,
+                                           user = list_italian_m5s[1:35],
+                                           
+                                           since_id = NULL,
+                                           max_id = NULL,
+                                           verbose = T, 
+                                           parse = T,
+) %>% 
+dplyr::filter(created_at > "2022-07-25" & created_at <="2022-09-24")
+
+
+#give me back only 400 tweets.
